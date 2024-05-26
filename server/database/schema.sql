21d3626bff31fa01bd2d66667bf0339bb5934517
sylvain.lafaye <sylvain-lafaye@hotmail.fr>
2024-05-26 17:09:23 +0200

    step5-harmonia

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -10,3 +10,18 @@ create table item (
   user_id int unsigned not null,
   foreign key(user_id) references user(id)
 );
+
+create table program (
+  id int unsigned primary key auto_increment not null,
+  title varchar(255) not null,
+  synopsis text not null,
+  poster VARCHAR(255) NOT NULL,
+  country VARCHAR(255) NOT NULL,
+  year INT NOT NULL,
+  category_id INT NOT NULL
+);
+
+create table category (
+  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+  Name VARCHAR(255) NOT NULL
+);
